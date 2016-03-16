a13fa40eef5d6767a7bd2bfea9526c75adc4183e
SeoAram <aram0710@hanmail.net>
2016-03-16 17:20:28 +0900

    수정중

--- server/server/ClientInfoManager.cpp
+++ server/server/ClientInfoManager.cpp
@@ -3,15 +3,8 @@
 
 
 ClientInfoManager::ClientInfoManager()
-:m_uConnCount(0)
+:m_uConnCount(0), m_bInit(false)
 {
-	//m_qLeaveId.emplace(MAX_CONNECT_CLIENT);
-
-	//boost::asio::ip::tcp::acceptor m_acceptor(boost::asio::io_service(), boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 31401));
-	for (unsigned int i = 0; i < MAX_CONNECT_CLIENT; ++i){
-		m_vClient.push_back((ClientInfo*)malloc( sizeof(ClientInfo)));
-		//m_vClient.push_back(new ClientInfo(i, m_acceptor.get_io_service()));
-	}
 }
 
 
@@ -22,9 +15,16 @@ ClientInfoManager::~ClientInfoManager()
 
 //accept에서 connect처리 해야 함
 ClientInfo* ClientInfoManager::connectClient(boost::asio::io_service& io_service){
-	ClientInfo c(getConnectCount(), io_service);
-	int i = getConnectCount();
-	memcpy(m_vClient[getConnectCount()], &c, sizeof(ClientInfo));
-	addConnextCount();
-	return m_vClient[i];
+	return nullptr;
+}
+
+bool ClientInfoManager::initializeClientInfoManager(boost::asio::io_service& io_service){
+	if (!m_bInit){
+		for (unsigned int i = 0; i < MAX_CONNECT_CLIENT; ++i){
+			m_vClient.push_back(new ClientInfo(i, io_service));
+		}
+		m_bInit = true;
+		return true;
+	}
+	return false;
 }
